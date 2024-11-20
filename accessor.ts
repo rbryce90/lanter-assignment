7c5695a5f3cf9ec99f61186689a11eadae6c016f
Bryce <brycerbond@gmail.com>
2024-11-20 09:05:16 -0600

    updated architure diagram

--- accessor.ts
+++ accessor.ts
@@ -16,12 +16,13 @@ export class AccessorClass {
     }
 
     async getFilmsByParams(sortField: "title" | "releaseYear", sortDirection: "ASC" | "DESC", pageSize: number, currentPage: number): Promise<Film[]> {
-
         if (currentPage) {
 
         }
 
+
         const url = `${this.url}/search`
+
         return await axios.post(url, {
             sortDirection,
             sortField,
